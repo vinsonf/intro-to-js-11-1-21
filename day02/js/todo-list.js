0b4a1492041e0cbbdb82ccef05bac9dba85d22a0
vinsonf <vfernandez@shift3tech.com>
2021-11-03 20:32:51 -0700

    day-2-todo-part1

---
+++ day02/js/todo-list.js
@@ -0,0 +1,21 @@
+let answer = 0;
+
+answer += 5;
+answer += 5;
+answer -= 2;
+answer *= 2;
+answer /= 4;
+
+// start of todo list
+// pull html elements into javascript
+const myInput = document.querySelector('#my-id');
+console.log(myInput);
+const myButton = document.querySelectorAll('.my-button')[0];
+console.log(myButton);
+
+
+// get value of input
+console.log(myInput.value);
+
+// add event listener to button
+myButton.addEventListener('click', function() {})
