c7813d4a32ce560eca21ea8d00a8f43b63fe67a4
vinsonf <vfernandez@shift3tech.com>
2021-11-03 21:04:08 -0700

    todo-with no comments or console logs

--- day02/js/todo-list.js
+++ day02/js/todo-list.js
@@ -1,19 +1,6 @@
-// pull html elements into javascript
 const myInput = document.querySelector('#my-id');
-console.log(myInput);
 const myButton = document.querySelectorAll('.my-button')[0];
-console.log(myButton);
 const myOl = document.querySelector('ol');
-console.log(myOl);
-
-// get value of input
-console.log(myInput.value);
-
-// add event listener to button
 myButton.addEventListener('dblclick', function() { 
-    // const li = document.createElement('li');
-    // li.innerHTML = myInput.value;
-    // myOl.append(li);
-
     myOl.innerHTML += `<li>${myInput.value}</li>`;
  });
